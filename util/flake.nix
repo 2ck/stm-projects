551098c8c8f212863a875e6bf080741c5ddb6114
2ck <y@2ck.me>
2025-06-26 23:11:15 +0200

    Make other flake multi-system too

--- util/flake.nix
+++ util/flake.nix
@@ -7,21 +7,27 @@
 
     outputs = { self, nixpkgs }:
     let
-        system = "x86_64-linux"; # should also work for "aarch64-linux" "aarch64-darwin" "x86_64-darwin"
-        pkgs = nixpkgs.legacyPackages.${system};
-        dependencies = [
-            (pkgs.python3.withPackages (pip3: [
-                pip3.pyserial
-                pip3.pyqt5
-                pip3.pillow
-            ]))
-            pkgs.libsForQt5.full
-
-        ];
+        systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" ];
+        forAll = nixpkgs.lib.attrsets.genAttrs;
     in
     {
-        devShells.${system}.default = pkgs.mkShell {
-            buildInputs = dependencies;
-        };
+        devShells = forAll systems (system:
+        let
+            pkgs = nixpkgs.legacyPackages.${system};
+            dependencies = [
+                (pkgs.python3.withPackages (pip3: [
+                    pip3.pyserial
+                    pip3.pyqt5
+                    pip3.pillow
+                ]))
+                pkgs.libsForQt5.full
+
+            ];
+        in
+        {
+            default = pkgs.mkShell {
+                buildInputs = dependencies;
+            };
+        });
     };
 }
